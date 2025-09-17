3f0ffc9f5cfa52bebde5c31035d8a1d2b8eb968f
psandesh30 <psandesh956@gmail.com>
2025-09-17 12:02:32 +0545

    login success

--- server/src/router/user.js
+++ server/src/router/user.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
-import { registerUser, verifyUser } from "../controller/user.js";
+import { loginUser, registerUser, verifyUser } from "../controller/user.js";
 
 const userRouter = Router();
 
 userRouter.route('/').post(registerUser);
 userRouter.route('/verify/:token').post(verifyUser)
-
+userRouter.route('/login').post(loginUser)
 export default userRouter;
